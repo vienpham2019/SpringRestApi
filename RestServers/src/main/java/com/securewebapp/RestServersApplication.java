5abab42ededc5ce2d060b1d49dc8f9112a167d9d
vienpham2019 <vienpham2019@gmail.com>
2021-04-07 14:53:27 -0500

    init commit

---
+++ RestServers/src/main/java/com/securewebapp/RestServersApplication.java
@@ -0,0 +1,13 @@
+package com.securewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServersApplication.class, args);
+	}
+
+}
---
+++ RestServers/src/test/java/com/securewebapp/RestServersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securewebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
